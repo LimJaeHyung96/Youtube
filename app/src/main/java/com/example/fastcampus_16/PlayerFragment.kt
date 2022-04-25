6fedaa7fc58c11fc6844df2e5d2b49239b5a317a
LimJaeHyung96 <jhlim96@naver.com>
2022-04-25 13:32:49 +0900

    220425 commit

--- app/src/main/java/com/example/fastcampus_16/PlayerFragment.kt
+++ app/src/main/java/com/example/fastcampus_16/PlayerFragment.kt
@@ -1,5 +1,6 @@
 package com.example.fastcampus_16
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -10,6 +11,11 @@ import com.example.fastcampus_16.adapter.VideoAdapter
 import com.example.fastcampus_16.databinding.FragmentPlayerBinding
 import com.example.fastcampus_16.dto.VideoDto
 import com.example.fastcampus_16.service.VideoService
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,8 +25,9 @@ import kotlin.math.abs
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
 
-    private var binding : FragmentPlayerBinding? = null
+    private var binding: FragmentPlayerBinding? = null
     private lateinit var videoAdapter: VideoAdapter
+    private var player: SimpleExoPlayer? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,6 +41,8 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
 
         initMotionLayoutEvent(fragmentPlayerBinding)
         initRecyclerView(fragmentPlayerBinding)
+        initPlayer(fragmentPlayerBinding)
+        initControlButton(fragmentPlayerBinding)
         getVideoList()
     }
 
@@ -80,6 +89,39 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
         }
     }
 
+    private fun initPlayer(fragmentPlayerBinding: FragmentPlayerBinding) {
+        context?.let { context ->
+            player = SimpleExoPlayer.Builder(context).build()
+        }
+
+        fragmentPlayerBinding.playerView.player = player
+        binding?.let{ binding ->
+            player?.addListener(object : Player.Listener {
+                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                    super.onIsPlayingChanged(isPlaying)
+
+                    if (isPlaying) {
+                        binding.bottomPlayerControlButton.setImageResource(R.drawable.ic_pause)
+                    } else {
+                        binding.bottomPlayerControlButton.setImageResource(R.drawable.ic_play)
+                    }
+                }
+            })
+        }
+    }
+
+    private fun initControlButton(fragmentPlayerBinding: FragmentPlayerBinding) {
+        fragmentPlayerBinding.bottomPlayerControlButton.setOnClickListener {
+            val player = this.player ?: return@setOnClickListener
+
+            if (player.isPlaying) {
+                player.pause()
+            } else {
+                player.play()
+            }
+        }
+    }
+
     private fun getVideoList() {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://run.mocky.io/")
@@ -90,7 +132,7 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
             it.listVideos()
                 .enqueue(object : Callback<VideoDto> {
                     override fun onResponse(call: Call<VideoDto>, response: Response<VideoDto>) {
-                        if(response.isSuccessful.not()){
+                        if (response.isSuccessful.not()) {
                             Log.d("MainActivity", "response fail")
                             return
                         }
@@ -98,6 +140,7 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
                             videoAdapter.submitList(videoDto.videos)
                         }
                     }
+
                     override fun onFailure(call: Call<VideoDto>, t: Throwable) {
                         //예외처리
                     }
@@ -106,16 +149,31 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
     }
 
     fun play(url: String, title: String) {
+
+        context?.let{ context ->
+            val dataSourceFactory = DefaultDataSourceFactory(context)
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(MediaItem.fromUri(Uri.parse(url)))
+            player?.setMediaSource(mediaSource)
+            player?.prepare()
+            player?.play()
+        }
+
         binding?.let {
             it.playerMotionLayout.transitionToEnd()
             it.bottomTitleTextView.text = title
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        player?.pause()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-
         binding = null
+        player?.release()
     }
 
 }
